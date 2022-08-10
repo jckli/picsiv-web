068d6726673e96f4e38346e0879188614685adc9
lina <jack@jackli.dev>
2022-08-10 15:33:36 -0700

    feat: first real initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,8 @@ const Home: NextPage = () => {
     return (
         <>
             <div className="w-full h-[100vh] flex flex-col items-center justify-center text-white text-3xl font-semibold text-center">
-                <h1 className="mb-4">jckli&apos;s next.js template</h1>
+                <h1>in development - picsiv web</h1>
+                <a className="text-[#494e53] transition-all ease-in-out duration-300 hover:text-[#a385b3] mb-6" href="/tos">terms of service</a>
                 <GitHubButton />
             </div>
         </>
